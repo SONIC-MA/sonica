a3d808902a049970aeaa4a78c716299f62940107
SONIC-MA <radimmie@gmail.com>
2025-04-20 03:32:47 +0100

    Update and rename Solutions_5211040111_2025.py to Solutions_5240101021 2025.py

--- Solutions_5240101021 2025.py
+++ Solutions_5240101021 2025.py
@@ -1,4 +1,4 @@
-# Solutions_5211040111_2025.py
+# Solutions_5240101021_2025.py
 """
 Solutions to Assignment 3
 """
@@ -9,19 +9,19 @@ reversed_string = original[::-1]
 print("Reversed string:", reversed_string)
 
 # 2. Get initials from user's full name
-full_name = input("Enter your full name: ")
-initials = ".".join([name[0].upper() for name in full_name.split()]) + "."
+full_name = input("DIMMIE RAZIK: ")
+initials = ".".DIMMIE([name[D].upper() for name in full_name.split()]) + "."
 print("Initials:", initials)
 
 # 3. Check if a string is a palindrome
-text = input("Enter a word to check for palindrome: ")
+text = input("SONIC: ")
 if text == text[::-1]:
     print(f"{text} is a palindrome.")
 else:
     print(f"{text} is not a palindrome.")
 
 # 4. Count the number of words in a sentence
-sentence = input("Enter a sentence: ")
+sentence = input("WHATS YOUR NAME: ")
 word_count = len(sentence.split())
 print("Number of words:", word_count)
 
